cf6a18c1c2494613660c0a1ab7484e4ad067594c
Scrappy03 <93342831+Scrappy03@users.noreply.github.com>
2025-03-12 19:05:55 +0000

    Refactor CSS build process and update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,43 @@
-const { DefaultDeserializer } = require('v8');
-
 module.exports = function (grunt) {
     const sass = require('sass');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks("grunt-concurrent");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin'); // Add this line
+
     grunt.registerTask('default', ['concurrent:watchall']);
     grunt.registerTask("vendors", ["uglify:vendor"]);
+    grunt.registerTask("build", ["sass:main", "cssmin", "uglify:main"]); // Add this line
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         sass: {
             main: {
                 options: {
                     sourceMap: true,
-                    outputStyle: 'compressed',
+                    outputStyle: 'expanded', // Use expanded for development
                     implementation: sass,
                 },
                 files: {
-                    './css/styles.css': './scss/styles.scss',
+                    './css/styles.css': './scss/styles.scss'
                 }
             },
         },
+
+        cssmin: {
+            target: {
+                files: {
+                    './css/styles.min.css': ['./css/styles.css']
+                }
+            }
+        },
+
         watch: {
             scss: {
                 files: ['./scss/**/*.scss'],
-                tasks: ['sass:main'],
+                tasks: ['sass:main', 'cssmin'], // Add cssmin here
                 options: {
                     spawn: false,
                 },
@@ -47,7 +59,7 @@ module.exports = function (grunt) {
                     mangle: false,
                 },
                 files: {
-                    './js/scripts.min.js': ["./scripts/**/*.js"],
+                    "./js/scripts.min.js": ["./scripts/**/*.js"],
                 },
             },
             vendor: {
@@ -57,12 +69,14 @@ module.exports = function (grunt) {
                     mangle: false,
                 },
                 files: {
-                    "./js/scripts-vendor.min.js": ["./node_modules/bootstrap/dist/js/bootstrap.min.js",
+                    "./js/scripts-vendor.min.js": [
+                        "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+                        "./node_modules/@glidejs/glide/dist/glide.min.js",
+                        "./node_modules/lightgallery/lightgallery.min.js"
                     ],
                 },
             },
         },
-
         concurrent: {
             options: {
                 logConcurrentOutput: true,
@@ -73,5 +87,4 @@ module.exports = function (grunt) {
             },
         },
     });
-}
-
+};
